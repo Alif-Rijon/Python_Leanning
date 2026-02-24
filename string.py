b320a5a5b3abb96f8b44d57dfc13253ddf3768b9
Alif Rijon <rijonsikder43@gmail.com>
2026-02-25 00:06:44 +0600

    opereator file add

--- string.py
+++ string.py
@@ -31,3 +31,40 @@ if "Rijon" in txt2:
 #check if not
 txt3= "My another name is Alif"
 print("Alif" not in txt3)
+
+#slicing
+
+b = "Hello world!"
+print(b[2:5]) #last index excluded
+print(b[:5])
+print(b[2:])
+print(b[-5:-2]) #start from last and -5 excluded and ended to -2 from last
+print("----")
+print(b[-5:])  
+print(b[:-2])
+print(b[:-5])
+print(b[-2:])
+
+#strip() method removed any whitespace from beginning or the end
+
+a= " Alif Rijon "
+print (a.strip())
+
+# replace and split returns a list 
+
+a= "Alif Rijon"
+print(a.replace("A" ,"a"))
+print(a.split(","))
+
+#F-string
+
+age=22
+txt= f"My age is {age}"
+print(txt)
+
+txt1 = f"It can take decimal point like {age:.3f}"
+print(txt1)
+
+txt2 = f"i can calculate math in placeholder {5*6}"
+print(txt2)
+print(txt.count("i"))
